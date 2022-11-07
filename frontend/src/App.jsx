578261103db61aa1585dc43cb16a1c6723482cd4
kenny <krogers@hey.com>
2022-11-07 15:25:44 -0700

    Code setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl">Hello Stacks</h1>
+    </div>
+  )
+}
+
+export default App
